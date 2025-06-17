82a06184a672211804d3e70cb34585d079e4bcdc
PiotrCzap <piotrczapmlawa@interia.pl>
2025-06-17 22:02:42 +0200

    Map Generator test 1

--- 2d-survival-game/Scripts/Map Generator.gd
+++ 2d-survival-game/Scripts/Map Generator.gd
@@ -1,12 +1,15 @@
 extends Node2D
 
-var size_x = 32
-var size_y = 32
+var size_x = 2
+var size_y = 2
+
 var Grass_chunk_scene = preload("res://Scenes/Chunks/grass_chunk.tscn")
-var bla = preload("res://icon.svg")
-var Grass_chunk = Grass_chunk_scene.instantiate()
 
 func _ready() -> void:
-	for x in range(size_x):
-		pass
 	
+	for x in range(-size_x,size_x):
+		for y in range(-size_y,size_y):
+			var grass_chunk = Grass_chunk_scene.instantiate()
+			grass_chunk.position += Vector2(x * 32,y * 32)
+			$"../Map".add_child(grass_chunk)
+			print(grass_chunk.position)
